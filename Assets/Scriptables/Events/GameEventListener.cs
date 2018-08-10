a2b3dff86ba165d225f96a183085ef2d37bdc3cd
Andrew Hoyt <andrew@chillhammer.com>
2018-08-09 23:48:22 -0400

    Scriptable Object Library and Sis Model
    Importing files

---
+++ Assets/Scriptables/Events/GameEventListener.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SO
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent gameEvent;
+        public UnityEvent response;
+
+        /// <summary>
+        /// Override this to override the OnEnableLogic()
+        /// </summary>
+        public virtual void OnEnableLogic()
+        {
+            if (gameEvent != null)
+                gameEvent.Register(this);
+        }
+
+        void OnEnable()
+        {
+            OnEnableLogic();
+        }
+
+        /// <summary>
+        /// Override this to override the OnDisableLogic()
+        /// </summary>
+        public virtual void OnDisableLogic()
+        {
+            if (gameEvent != null)
+                gameEvent.UnRegister(this);
+        }
+
+        void OnDisable()
+        {
+            OnDisableLogic();
+        }
+
+        public virtual void Response()
+        {
+            response.Invoke();
+        }
+    }
+}
---
+++ Assets/Scriptables/UI/UIPropertyUpdater.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SO.UI
+{
+    public class UIPropertyUpdater : GameEventListener
+    {
+        /// <summary>
+        /// Use this to update the UI element as soon as THIS gameObject is enabled
+        /// </summary>
+        public bool raiseOnEnable;
+        /// In the off chance you need to update a UI element when disabled, just add the OnDisable() method
+
+        /// <summary>
+        /// If there's a gameEvent assigned it will automaticall call the Raise() method.
+        /// </summary>
+
+        public override void Response()
+        {
+            if (gameEvent != null)
+                Raise();
+        }
+
+        public virtual void Raise()
+        {
+            
+        }
+
+        public override void OnEnableLogic()
+        {
+            base.OnEnableLogic();
+            if(raiseOnEnable)
+            {
+                Raise();
+            }
+        }
+    }
+}
