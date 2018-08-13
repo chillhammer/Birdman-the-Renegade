79d05138452287e3547015805edfc7df820f59be
Andrew Hoyt <andrew@chillhammer.com>
2018-08-13 04:49:21 -0400

    Cursor Replaced
    Simple 4 prong cursor replaces mouse cursor, camera repositioned slightly

---
+++ Assets/SisGame/Cursor/CursorManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SIS.Actions.Input;
+
+namespace SIS.Managers
+{
+	[DisallowMultipleComponent]
+	public class CursorManager : MonoBehaviour
+	{
+		[SerializeField] private InputAxis mouseX;
+		[SerializeField] private InputAxis mouseY;
+
+
+		// Use this for initialization
+		void Start()
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
