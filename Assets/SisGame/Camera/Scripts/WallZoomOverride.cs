6d8b500a39da1dfe851d24433ef879253019bfe0
Andrew Hoyt <andrew@chillhammer.com>
2018-08-22 13:08:00 -0400

    Simple Camera Move Out of Wall
    Need to make it more smooth

---
+++ Assets/SisGame/Camera/Scripts/WallZoomOverride.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SIS.Actions;
+
+namespace SIS.Camera
+{
+	[CreateAssetMenu(menuName = "Actions/Mono Actions/Camera/Wall Zoom Override")]
+	public class WallZoomOverride : Action
+	{
+		public SO.TransformVariable pivotTransform;
+		public SO.TransformVariable actualCameraTransform;
+		public LayerMask wallLayer;
+
+		public override void Execute()
+		{
+			Vector3 dir = actualCameraTransform.value.position - pivotTransform.value.position;
+			float dist = dir.magnitude;
+			Ray ray = new Ray(pivotTransform.value.position, dir);
+
+			RaycastHit raycastHit;
+			if (Physics.Raycast(ray, out raycastHit, dist, wallLayer))
+			{
+				//Debug.Log("Hitting Wall");
+				/*
+				Vector3 localHit = actualCameraTransform.value.InverseTransformPoint(raycastHit.point);
+				Vector3 localPos = actualCameraTransform.value.localPosition;
+				localPos.z = localHit.z;
+				actualCameraTransform.value.localPosition = localPos;
+				*/
+				actualCameraTransform.value.position = raycastHit.point;
+			}
+		}
+	}
+}
