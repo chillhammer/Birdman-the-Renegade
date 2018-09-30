69769e6b8e9f9e51422ecdb627489136a25059dc
Andrew Hoyt <andrew@chillhammer.com>
2018-09-29 21:43:04 -0400

    Weapon Tip Alignment Action
    Need to remove Weapon Tip from Weapon Parent

---
+++ Assets/SisGame/Characters/Sis/Scripts/Mono Actions/WeaponTipAlignment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SIS.Actions;
+
+namespace SIS.Characters.Sis
+{
+	[CreateAssetMenu(menuName ="Actions/Mono Actions/Weapon Tip Alignment")]
+	public class WeaponTipAlignment : Action
+	{
+		public SisVariable sis;
+
+		public override void Execute()
+		{
+			if (sis.value != null)
+			{
+				Items.Weapons.Weapon.RuntimeWeapon runtime = sis.value.inventory.currentWeapon.runtime;
+
+				Vector3 origin = (runtime.weaponTip == null ? runtime.modelInstance.transform.position : runtime.weaponTip.position);
+				Vector3 dir = sis.value.movementValues.aimPosition - runtime.weaponTip.position;
+
+				//Actual Alignment
+				runtime.weaponTip.rotation = Quaternion.LookRotation(dir);
+
+			} else
+			{
+				Debug.LogWarning("Sis not defined!");
+			}
+		}
+	}
+}
