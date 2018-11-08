cafa77bb71d407670659045e1eb425c1f2199627
HoldtightAsnee <43424291+HoldtightAsnee@users.noreply.github.com>
2018-11-07 20:03:29 -0500

    Bill death, attack, sound effects.

--- Assets/SisGame/Characters/Bill/Bill.cs
+++ Assets/SisGame/Characters/Bill/Bill.cs
@@ -10,14 +10,24 @@ namespace SIS.Characters.Bill
         SO.TransformVariable playerTransform;
         [SerializeField]
         LayerMask visionMask;
+        [SerializeField]
+        AudioSource audioSource;
+        [SerializeField]
+        AudioClip angrySound;
+        [SerializeField]
+        Sis.SisVariable sisVariable;
+        [SerializeField]
+        AnimationCurve deathSizeCurve;
         WaypointNavigator waypointNavigator;
         CharacterController charactercontroller;
 
         private float timer = 0f;
+        public bool dead = false;
         [SerializeField]
         private bool angry = false;
         public float normalspeed = 1;
         public float angryspeed = 4;
+        public float hitDistance = 1f;
         // Use this for initialization
         void Start()
         {
@@ -29,24 +39,56 @@ namespace SIS.Characters.Bill
         // Update is called once per frame
         void Update()
         {   
-            if (playerTransform.value != null)
+            if (dead)
             {
                 timer += Time.deltaTime;
-                if (timer > 0.5f)
-                {
-                    timer = 0;
-                    waypointNavigator.StartNavigation(playerTransform.value.position);
-                    // First time seeing the player.
-                    if (angry == false)
-                    { 
-                        if (CanSeePlayer())
-                        {
-                            angry = true;
-                            Debug.Log("Bill saw the player");
 
+                float deathTime = 2f;
+                float deathSize = deathSizeCurve.Evaluate(timer / deathTime);
+                transform.localScale = new Vector3(deathSize, deathSize, deathSize);
+
+                if (timer >= deathTime)
+                {
+                    Destroy(gameObject);
+                }
+                return;
+            }
+            if (playerTransform.value != null)
+            {
+                //If There is a Straight Path
+                if (angry && waypointNavigator.PathFinished && CanSeePlayer())
+                {
+                    //Naive Pathfinding
+                    Vector3 dirtoplayer = playerTransform.value.position - transform.position;
+                    charactercontroller.Move(angryspeed * dirtoplayer * Time.deltaTime);
+                    dirtoplayer.y = 0;
+                    Quaternion quatdir = Quaternion.LookRotation(dirtoplayer);
+                    float rotationspeed = 5f;
+                    transform.rotation =
+                        Quaternion.Slerp(transform.rotation, quatdir, rotationspeed * Time.deltaTime);
+                }
+                else
+                {
+                    //Smart pathfinding
+                    timer += Time.deltaTime;
+                    if (timer > 0.5f)
+                    {
+                        timer = 0;
+                        waypointNavigator.StartNavigation(playerTransform.value.position);
+                        // First time seeing the player.
+                        if (angry == false)
+                        {
+                            if (CanSeePlayer())
+                            {
+                                angry = true;
+                                audioSource.PlayOneShot(angrySound, 0.1f);
+                                //Debug.Log("Bill saw the player");
+
+                            }
                         }
                     }
                 }
+                
             }
             
             // Movement and rotation.
@@ -55,12 +97,19 @@ namespace SIS.Characters.Bill
                 float speed = (angry ? angryspeed : normalspeed);
                 Vector3 dir = waypointNavigator.DirectionToWaypoint;
                 charactercontroller.Move(dir * speed * Time.deltaTime);
+                dir.y = 0;
                 Quaternion quatdir = Quaternion.LookRotation(dir);
                 float rotationspeed = 2.0f;
                 transform.rotation =
                     Quaternion.Slerp(transform.rotation, quatdir, rotationspeed * Time.deltaTime);
             }
+            //Hit Sis!
+            if (Vector3.Distance(transform.position, playerTransform.value.position) < hitDistance)
+            {
+                sisVariable.value.OnHit(null, 3f, Vector3.zero, Vector3.zero);
+                SetDead();
 
+            }
         }
 
         bool CanSeePlayer()
@@ -81,18 +130,29 @@ namespace SIS.Characters.Bill
         public void OnHit(Character shooter, float baseDamage, Vector3 dir, Vector3 pos)
         {
             //To do: Create Explosion, death animation.
-            Destroy(gameObject);
+            SetDead();
         }
 
         public bool IsDead()
         {
-            return false;
+            return dead;
             
+        }
+
+        void SetDead()
+        {
+            dead = true;
+            timer = 0;
         }
 
         public void PlaySound(AudioClip audio)
         {
-            //To do: add audio source component.
+            audioSource.PlayOneShot(audio);
+        }
+
+        public void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position, hitDistance);
         }
     }
 }
